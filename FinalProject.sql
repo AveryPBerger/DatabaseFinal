385fce8ce9d21e876e5be866b5219e366fb22d34
Avery Berger <averyberger@icloud.com>
2024-12-15 17:18:33 -0500

    Started building db

--- FinalProject.sql
+++ FinalProject.sql
@@ -1,1 +1,37 @@
+CREATE DATABASE BlueWood;
 
+USE BlueWood;
+
+CREATE TABLE Cabins(
+	CabinID int PRIMARY KEY,
+	CabinName varchar(500),
+	MaxCapacity int
+	)
+
+CREATE TABLE Country(
+	CountryID int PRIMARY KEY,
+	CountryName varchar(500)
+	)
+CREATE TABLE Province(
+	ProvinceID int PRIMARY KEY,
+	ProvinceName varchar(500)
+	)
+
+CREATE TABLE Activities(
+	ActivityID int PRIMARY KEY,
+	ActivityName varchar(500),
+	MaxCapacity int
+	)
+CREATE TABLE EmergencyContact(
+	EmergencyContactID int PRIMARY KEY,
+	FirstName varchar(500),
+	LastName varchar(500),
+	PhoneNumber char(10),
+	CivicNumber int,
+	StreetName varchar(500),
+	City varchar(500),
+	ProvinceID int,
+	CountryID int,
+	FOREIGN KEY (ProvinceID) REFERENCES Province(ProvinceID),
+	FOREIGN KEY (CountryID) REFERENCES Country(CountryID)
+	)
