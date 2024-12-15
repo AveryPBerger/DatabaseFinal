af1ca6476f0acd0559fb6a8ba63be1dddbf99448
Avery Berger <averyberger@icloud.com>
2024-12-15 18:17:21 -0500

    Finished base table making except salary and councilor

--- FinalProject.sql
+++ FinalProject.sql
@@ -1,8 +1,7 @@
 CREATE DATABASE BlueWood;
-
+DROP DATABASE BlueWood;
 USE BlueWood;
-
-CREATE TABLE Cabins(
+CREATE TABLE Cabin(
 	CabinID int PRIMARY KEY,
 	CabinName varchar(500),
 	MaxCapacity int
@@ -16,8 +15,7 @@ CREATE TABLE Province(
 	ProvinceID int PRIMARY KEY,
 	ProvinceName varchar(500)
 	)
-
-CREATE TABLE Activities(
+CREATE TABLE Activity(
 	ActivityID int PRIMARY KEY,
 	ActivityName varchar(500),
 	MaxCapacity int
@@ -34,4 +32,39 @@ CREATE TABLE EmergencyContact(
 	CountryID int,
 	FOREIGN KEY (ProvinceID) REFERENCES Province(ProvinceID),
 	FOREIGN KEY (CountryID) REFERENCES Country(CountryID)
+	)
+CREATE TABLE Camper(
+	CamperID int PRIMARY KEY,
+	FirstName varchar(500),
+	LastName varchar(500),
+	Age int,
+	YearsAtCamp smallint,
+	CabinID int,
+	FOREIGN KEY (CabinID) REFERENCES Cabin(CabinID)
+	)
+CREATE TABLE CamperEmergencyContact(
+	CamperID int,
+	EmergencyContactID int,
+	PRIMARY KEY(CamperID,EmergencyContactID),
+	FOREIGN KEY (CamperID) REFERENCES Camper(CamperID),
+	FOREIGN KEY (EmergencyContactID) REFERENCES EmergencyContact(EmergencyContactID)
+	)
+CREATE TABLE CamperActivity(
+	CamperID int,
+	ActivityID int,
+	PRIMARY KEY(CamperID,ActivityID),
+	FOREIGN KEY (CamperID) REFERENCES Camper(CamperID),
+	FOREIGN KEY (ActivityID) REFERENCES Activity(ActivityID)
+	)
+CREATE TABLE Allergie(
+	AllergieID int,
+	AllergieName varchar(500),
+	CamperID int,
+	FOREIGN KEY (CamperID) REFERENCES Camper(CamperID)
+	)
+CREATE TABLE Medication(
+	MedicationID int,
+	MedicationName varchar(500),
+	CamperID int,
+	FOREIGN KEY (CamperID) REFERENCES Camper(CamperID)
 	)
